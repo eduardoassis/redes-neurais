291eea4893626327e8ba499adf12110cd9273778
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-02-05 22:25:06 -0200

    Implementação de perceptron de uma camada com treinamento e ajuste de pesos

---
+++ errorCorrection.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 21:21:25 2018
+@author: eduardo
+"""
+import numpy as np
+
+entradas = np.array([[0,0], [0,1], [1,0], [1,1]])
+saidas = np.array([0, 0, 0, 1])
+pesos = np.array([0.0, 0.0])
+taxaDeAprendizagem = 0.1
+
+def stepFunction(soma):
+    if(soma >= 1):
+        return 1
+    return 0
+
+def calculaSaida(registro):
+    s = registro.dot(pesos)
+    return stepFunction(s)
+
+def treinar():
+    erroTotal = 1
+    while(erroTotal != 0):
+        erroTotal = 0
+        for i in range(len(saidas)):
+            saidaCalculada = calculaSaida(np.asanyarray(entradas[i]))
+            erro = abs(saidas[i] - saidaCalculada)
+            erroTotal += erro
+            for j in range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaDeAprendizagem * entradas[i][j] * erro)
+                print('Peso atualizado: ' + str(pesos[j]))
+        print('Total de erros: ' + str(erroTotal))
+            
+treinar()
+                
