10323f672c889ad87a06ef4c90acdd00ff9e9fb4
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-02-11 10:50:27 -0200

    Adição de caso para simular a operação lógica OR e ajuste no retorna da função 'calculaSaida'

--- errorCorrection.py
+++ errorCorrection.py
@@ -7,7 +7,8 @@ Created on Mon Feb  5 21:21:25 2018
 import numpy as np
 
 entradas = np.array([[0,0], [0,1], [1,0], [1,1]])
-saidas = np.array([0, 0, 0, 1])
+#saidas = np.array([0, 0, 0, 1])
+saidas = np.array([0, 1, 1, 1])
 pesos = np.array([0.0, 0.0])
 taxaDeAprendizagem = 0.1
 
@@ -17,7 +18,7 @@ def stepFunction(soma):
     return 0
 
 def calculaSaida(registro):
-    s = registro.dot(pesos)
+    s = round(registro.dot(pesos), 2)
     return stepFunction(s)
 
 def treinar():
@@ -29,8 +30,8 @@ def treinar():
             erro = abs(saidas[i] - saidaCalculada)
             erroTotal += erro
             for j in range(len(pesos)):
-                pesos[j] = pesos[j] + (taxaDeAprendizagem * entradas[i][j] * erro)
-                print('Peso atualizado: ' + str(pesos[j]))
+                    pesos[j] = pesos[j] + (taxaDeAprendizagem * entradas[i][j] * erro)
+                    print('Peso atualizado: ' + str(pesos[j]))
         print('Total de erros: ' + str(erroTotal))
             
 treinar()
