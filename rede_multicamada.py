e02adf3252d2b56834eea605ccf4153e8ff45edf
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-04-10 00:25:31 -0300

    Cálculo da camada de saída

--- rede_multicamada.py
+++ rede_multicamada.py
@@ -17,11 +17,13 @@ saidas = np.array([[0], [1], [1], [0]])
 pesos0 = np.array([[-0.424, -0.740, -0.961],
                    [0.358, -0.577, -0.469]])
     
-pesos1 = np.array([[-0.017, -0.893, 0.148]])
+pesos1 = np.array([[-0.017], [-0.893], [0.148]])
 
 epocas = 100
 
 for j in range(epocas):
     camadaEntrada = entradas
-    somaSinopse0 = np.dot(camadaEntrada, pesos0)
-    camadaOculta = sigmoid(somaSinopse0)
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
+    camadaOculta = sigmoid(somaSinapse0)
+    somaSinapse1 = np.dot(camadaOculta, pesos1)
+    camadaSaida = sigmoid(somaSinapse1)
